a84402ff423d2a998e3ec58e87b72fc8c676b6e2
Andrew Roberts <a.roberts@student.saedubai.com>
2024-04-15 00:23:08 +0400

    Match Manager Script And Inheriting Variables from Death Script

---
+++ Assets/Game Falliing Legacy/Scripts/Andrew/MatchManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    public Death p1Lives;
+    public Death p2Lives;
+
+    public GameObject p1WinUI;
+    public GameObject p2WinUI;
+
+    //Needs the death border prefab with Death script attached
+    public GameObject deathBound;
+
+    private void Awake()
+    {
+        p1WinUI.SetActive(false);
+        p2WinUI.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1Lives = deathBound.GetComponent<Death>();
+        p2Lives = deathBound.GetComponent<Death>();
+    }
+
+    public void WinCondition()
+    {
+        if (p1Lives.player1Lives <= 0)
+        {
+            p1WinUI.SetActive(true);
+        }
+
+        if (p2Lives.player2Lives <= 0)
+        {
+            p2WinUI.SetActive(true);
+        }
+    }
+}
