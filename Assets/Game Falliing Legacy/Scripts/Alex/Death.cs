e5adfe502e9c76a6b97f3956b3c2c5743e7dad6c
AlexIdris <146952700+AlexIdris@users.noreply.github.com>
2024-04-22 15:40:54 +0400

    Making BUILDABLE?!!!

--- Assets/Game Falliing Legacy/Scripts/Alex/Death.cs
+++ Assets/Game Falliing Legacy/Scripts/Alex/Death.cs
@@ -4,13 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class Death : MatchManager
 {
+    [SerializeField] AudioSource kO;
     [SerializeField] Animator animator1;
     [SerializeField] Animator animator2;
+    [SerializeField] GameObject outOfSight;
     bool stillin1 = false;
     bool stillin2 = false;
     public int player1Lives = 5;
     public int player2Lives = 5;
-
+    private void Start()
+    {
+        outOfSight.SetActive(false);
+    }
     async private void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Player 1")
@@ -20,8 +25,11 @@ public class Death : MatchManager
 
             if (stillin1)
             {
+                kO.Play();
+                outOfSight.SetActive(true);
+
                 animator1.SetBool("Death", true);
-                await Task.Delay(1000);
+                await Task.Delay(1200);
                 Destroy(other.gameObject);
                 player1Lives--;
                 WinCondition();
@@ -30,6 +38,8 @@ public class Death : MatchManager
             else if (!stillin1)
             {
                 stillin1 = false;
+                outOfSight.SetActive(false);
+
             }
 
 
@@ -41,8 +51,11 @@ public class Death : MatchManager
 
             if (stillin2)
             {
+                kO.Play();
+                outOfSight.SetActive(true);
+
                 animator2.SetBool("Death", true);
-                await Task.Delay(1000);
+                await Task.Delay(1200);
                 Destroy(other.gameObject);
                 player2Lives--;
                 WinCondition();
@@ -51,6 +64,8 @@ public class Death : MatchManager
             else if (!stillin2)
             {
                 stillin2 = false;
+                outOfSight.SetActive(false);
+
             }
         }
 
@@ -61,12 +76,15 @@ public class Death : MatchManager
         {
 
             stillin1 = false;
+            outOfSight.SetActive(false);
 
         }
         if (other.tag == "Player 2")
         {
 
             stillin2 = false;
+            outOfSight.SetActive(false);
+
         }
     }
 }
