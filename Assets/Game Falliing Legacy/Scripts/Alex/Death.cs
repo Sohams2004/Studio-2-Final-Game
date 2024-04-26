28dfefe540fd1f42dfa66cd2af9398a6ac716f5b
AlexIdris <146952700+AlexIdris@users.noreply.github.com>
2024-04-26 20:27:30 +0400

    Respawnding and health system

--- Assets/Game Falliing Legacy/Scripts/Alex/Death.cs
+++ Assets/Game Falliing Legacy/Scripts/Alex/Death.cs
@@ -12,11 +12,26 @@ public class Death : MatchManager
     bool stillin2 = false;
     public int player1Lives = 5;
     public int player2Lives = 5;
+
+    [SerializeField] private Transform respawnLocation1;
+    [SerializeField] private Transform respawnLocation2;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    [SerializeField] GameObject player1health1;
+    [SerializeField] GameObject player1health2;
+    [SerializeField] GameObject player1health3;
+    [SerializeField] GameObject player1health4;
+
+    [SerializeField] GameObject player2health1;
+    [SerializeField] GameObject player2health2;
+    [SerializeField] GameObject player2health3;
+    [SerializeField] GameObject player2health4;
     private void Start()
     {
         outOfSight.SetActive(false);
     }
-    async private void OnTriggerStay2D(Collider2D other)
+    async private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player 1")
         {
@@ -26,16 +41,41 @@ public class Death : MatchManager
             if (stillin1)
             {
 
-                animator1.SetBool("Death", true);
+                animator1.SetTrigger("Death");
                 await Task.Delay(1200);
-                Destroy(other.gameObject);
+
                 player1Lives--;
                 WinCondition();
+                if (player1Lives == 4)
+                {
+                    player1health4.SetActive(false);
+                    player1.transform.position = respawnLocation1.transform.position;
+                    Physics2D.SyncTransforms();
+                }
 
-                if (player1Lives == 0)
+                else if (player1Lives == 3)
+                {
+                    player1health3.SetActive(false);
+                    player1.transform.position = respawnLocation1.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+                else if (player1Lives == 2)
+                {
+                    player1health2.SetActive(false);
+                    player1.transform.position = respawnLocation1.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+                if (player1Lives == 1)
+                {
+                    player1health1.SetActive(false);
+                    player1.transform.position = respawnLocation1.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+                else if (player1Lives == 0)
                 {
 
                     kO.Play();
+                    Destroy(other.gameObject);
                     outOfSight.SetActive(true);
                     await Task.Delay(2000);
                     SceneManager.LoadScene(1);
@@ -45,6 +85,8 @@ public class Death : MatchManager
             {
                 stillin1 = false;
                 outOfSight.SetActive(false);
+                animator1.ResetTrigger("Death");
+                await Task.Delay(400);
 
             }
 
@@ -57,16 +99,43 @@ public class Death : MatchManager
 
             if (stillin2)
             {
-                animator2.SetBool("Death", true);
+                animator2.SetTrigger("Death");
                 await Task.Delay(1200);
-                Destroy(other.gameObject);
+
                 player2Lives--;
                 WinCondition();
 
-                if (player2Lives == 0)
+                if (player2Lives == 4)
+                {
+                    player2health4.SetActive(false);
+
+                    player2.transform.position = respawnLocation2.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+
+                else if (player2Lives == 3)
+                {
+                    player2health3.SetActive(false);
+                    player2.transform.position = respawnLocation2.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+                else if (player2Lives == 2)
+                {
+                    player2health2.SetActive(false);
+                    player2.transform.position = respawnLocation2.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+                if (player2Lives == 1)
+                {
+                    player2health1.SetActive(false);
+                    player2.transform.position = respawnLocation2.transform.position;
+                    Physics2D.SyncTransforms();
+                }
+                else if (player2Lives == 0)
                 {
 
                     kO.Play();
+                    Destroy(other.gameObject);
                     outOfSight.SetActive(true);
                     await Task.Delay(2000);
                     SceneManager.LoadScene(1);
@@ -76,26 +145,29 @@ public class Death : MatchManager
             {
                 stillin2 = false;
                 outOfSight.SetActive(false);
-
+                animator2.ResetTrigger("Death");
+                await Task.Delay(400);
             }
         }
 
     }
-    private void OnTriggerExit2D(Collider2D other)
+    async private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player 1")
         {
 
             stillin1 = false;
             outOfSight.SetActive(false);
-
+            animator1.ResetTrigger("Death");
+            await Task.Delay(400);
         }
         if (other.tag == "Player 2")
         {
 
             stillin2 = false;
             outOfSight.SetActive(false);
-
+            animator2.ResetTrigger("Death");
+            await Task.Delay(400);
         }
     }
 }
